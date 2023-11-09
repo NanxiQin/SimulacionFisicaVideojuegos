02da508b013095ab3fe7577826bf8874ace95dac
NanxiQin <nanxiqin@ucm.es>
2023-11-09 13:15:09 +0100

    viento area

--- skeleton/WindForceGenerator.cpp
+++ skeleton/WindForceGenerator.cpp
@@ -1,16 +1,18 @@
 #include "WindForceGenerator.h"
 
 
-WindForceGenerator::WindForceGenerator(double k1, Vector3 windVel, double duration ) :ForceGenerator(WindForce,duration), windVel(windVel),k1(k1),airDensity(1.2),dragCoef(0.5),k2(0.1){}
+WindForceGenerator::WindForceGenerator(double k1, Vector3 windVel, double duration,Vector3 windArea) :ForceGenerator(WindForce,duration), windVel(windVel),k1(k1),airDensity(1.2),dragCoef(0.5),k2(0.1), windArea(windArea){}
 
 void WindForceGenerator::updateForce(Particle* particle, double t)
 {
 	//si tiene masa infinita
 	if (fabs(particle->getInvMass()) < 1e-10) return;
 	
+	auto pPos = particle->getPos();
+	if(fabs(pPos.x) > windArea.x || fabs(pPos.y) > windArea.y || fabs(pPos.z) > windArea.y) return; //si está fuera del área
+
 	double areaEffe = 4 *PI * pow(particle->getRadius(), 2); //área efectiva del objeto que se enfrenta al viento 
 	k2 = areaEffe * dragCoef* airDensity; 
-
 
 	Vector3 v = windVel- particle->getVel();
 	// Apply the wind force
--- skeleton/WindForceGenerator.h
+++ skeleton/WindForceGenerator.h
@@ -4,17 +4,19 @@ class WindForceGenerator : public ForceGenerator
 {
 public:
 	//NOTA: evitar masa muy pequeña con radio muy grande
-	WindForceGenerator(double k1 = 0, Vector3 windVel = { 0,0,0 },double duration=-1);
+	WindForceGenerator(double k1 = 0, Vector3 windVel = { 0,0,0 }, double duration = -1, Vector3 windArea = { 50,50,50 });
 	virtual void updateForce(Particle* particle, double t);
 	inline void setDrag(double k1_) { k1 = k1_; }
 	inline double getK1() { return k1; }
 protected:
 	Vector3 windVel;
-	
+	Vector3 windArea;
+
 	const double airDensity;
 	const double dragCoef;
 	double k1; //coef rozamiento vel
 	double k2; //coef square vel
+
 
 };
 
